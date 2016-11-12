9388d5f2d7f78f7cf9e541b140a384b44d1a0322
hydroxyzinum <przemek.prokopczuk1@gmail.com>
2016-11-12 07:39:16 +0100

    Final commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      redirect_to root_path
+      redirect_to @user
     else
       render 'new'
     end
